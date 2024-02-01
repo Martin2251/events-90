88c9521d2006fe3464dfe7ef793e21970239ed68
Martin Lowe <martinlowe673@gmail.com>
2024-02-01 07:57:01 +0000

    improved header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header className='w-full border-b'>
+        <div className='wrapper flex items-center justify-between'>
+            <Link href="/" className='w-36'>
+                <Image src="/assets/images/logo.svg" width={128} height={38} alt="logo"/>
+            </Link>
+        </div>
+    </header>
   )
 }
 
